b2fa84ad6ef17d9c9a28b18341013d0858974b38
dahunkim <kim23222@naver.com>
2020-06-04 22:12:10 +0900

    [테스트] 테스트 코드 작성
    EventControllerTests - Valid 테스트
     입력 받을 수 없는 값 주입 시 에러 테스트
     필수 입력 값 입력 하지 않았을 경우 에러 테스트
     잘못 된 입력 값 검증 테스트
    EventDto - 필요 없는 값 옮겨 담기
    TestDescription - 커스텀 어노테이션

--- src/test/java/xyz/dahun/events/EventControllerTests.java
+++ src/test/java/xyz/dahun/events/EventControllerTests.java
@@ -1,18 +1,19 @@
 package xyz.dahun.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import xyz.dahun.common.TestDescription;
 
 import java.time.LocalDateTime;
 
@@ -20,8 +21,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class EventControllerTests {
 
     @Autowired
@@ -30,13 +32,14 @@ public class EventControllerTests {
     @Autowired
     ObjectMapper objectMapper;
 
-    @MockBean
-    EventRepository eventRepository;
+//    @MockBean
+//    EventRepository eventRepository;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
 
-        Event event = Event.builder()
+        EventDto event = EventDto.builder()
                 .name("spring")
                 .description("REST API Development with Spring")
                 .beginEnrollmentDateTime(LocalDateTime.of(2020, 11, 23, 14, 15))
@@ -49,9 +52,9 @@ public class EventControllerTests {
                 .location("강남역 D2 스타트업 팩토리")
                 .build();
 
-        event.setId(10);
+//        event.setId(10);
 
-        Mockito.when(eventRepository.save(event)).thenReturn(event);
+//        Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -61,6 +64,77 @@ public class EventControllerTests {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("id").exists())
                 .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+    }
+
+    @Test
+    @TestDescription("입력 받을 수 없는 값 주입 시 에러 발생")
+    public void createEvent_Bad_Request() throws Exception {
+
+        Event event = Event.builder()
+                .id(100)
+                .name("spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020, 11, 23, 14, 15))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020, 11, 24, 15, 51))
+                .beginEventDateTime(LocalDateTime.of(2020, 11, 25, 14, 21))
+                .endEventDateTime(LocalDateTime.of(2020, 11, 26, 14, 26))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타트업 팩토리")
+                .free(true)
+                .offline(false)
+                .eventStatus(EventStatus.PUBLISHED)
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("필수 입력 값에 입력하지 않았을 경우")
+    public void createEvent_Bad_Reqeust_Empty_Input() throws Exception {
+        EventDto eventDto = EventDto.builder().build();
+
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("잘못 된 입력 값을 검증하는 테스트")
+    public void createEvent_Bad_Reqeust_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020, 11, 26, 14, 15))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020, 11, 25, 15, 51))
+                .beginEventDateTime(LocalDateTime.of(2020, 11, 24, 14, 21))
+                .endEventDateTime(LocalDateTime.of(2020, 11, 23, 14, 26))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타트업 팩토리")
+                .build();
+
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest())
+                .andDo(print())
+                .andExpect(jsonPath("$[0].objectName").exists())
+//                .andExpect(jsonPath("$[0].field").exists())
+                .andExpect(jsonPath("$[0].defaultMessage").exists())
+                .andExpect(jsonPath("$[0].code").exists());
+//                .andExpect(jsonPath("$[0].rejectedValue").exists());
     }
 }
