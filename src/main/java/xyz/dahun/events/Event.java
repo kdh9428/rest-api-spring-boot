356c2e1c7adbf311dbc3f3cd3903118a8626d317
dahunkim <kim23222@naver.com>
2020-06-05 22:25:17 +0900

    [기능 추가] 지역, 가격 기능
    지역을 지정 했을 경우 오프라인으로 설정
    가격을 지정 했을 경우 free필드 false, 지정하지 않았을 경우 true

--- src/main/java/xyz/dahun/events/Event.java
+++ src/main/java/xyz/dahun/events/Event.java
@@ -6,12 +6,17 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Builder @AllArgsConstructor @NoArgsConstructor
-@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
 @Entity
 public class Event {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Integer id;
     private String name;
     private String description;
@@ -28,4 +33,23 @@ public class Event {
     @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
 
+    public void update() {
+
+        // Update Free
+        if (this.basePrice == 0 && this.maxPrice == 0) {
+            this.free = true;
+        } else {
+            this.free = false;
+        }
+
+        // Update offline
+        if(this.location == null || this.location.isBlank()){
+            this.offline = false;
+        } else {
+            this.offline = true;
+        }
+
+
+
+    }
 }
--- src/test/java/xyz/dahun/events/EventTest.java
+++ src/test/java/xyz/dahun/events/EventTest.java
@@ -1,5 +1,6 @@
 package xyz.dahun.events;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,5 +30,72 @@ class EventTest {
         //Then
         assertThat(event.getName()).isEqualTo(name);
         assertThat(event.getDescription()).isEqualTo(description);
+    }
+
+    @Test
+    @DisplayName("가격이 있거나 없으면")
+    public void testFree(){
+
+        // Given
+        Event event = Event.builder()
+                .basePrice(0)
+                .maxPrice(0)
+                .build();
+
+        // When
+        event.update();
+
+        //Then
+        assertThat(event.isFree()).isTrue();
+
+        // Given
+        event = Event.builder()
+                .basePrice(100)
+                .maxPrice(0)
+                .build();
+
+        // When
+        event.update();
+
+        //Then
+        assertThat(event.isFree()).isFalse();
+
+        // Given
+        event = Event.builder()
+                .basePrice(0)
+                .maxPrice(100)
+                .build();
+
+        // When
+        event.update();
+
+        //Then
+        assertThat(event.isFree()).isFalse();
+    }
+
+    @Test
+    @DisplayName("오프라인 모임 테스트")
+    public void testOffline(){
+        // Given
+        Event event = Event.builder()
+                .location("강남역 네이버 D2")
+                .build();
+
+        // When
+        event.update();
+
+        //Then
+        assertThat(event.isOffline()).isTrue();
+
+        // Given
+        event = Event.builder()
+                .build();
+
+        // When
+        event.update();
+
+        //Then
+        assertThat(event.isOffline()).isFalse();
+
     }
 }
